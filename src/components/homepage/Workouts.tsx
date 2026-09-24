42a99b43d00e45c1c85829abbcebdb79fc5dee2b
yeasinkhan08 <kyeasin58@gmail.com>
2026-09-24 22:46:54 +0600

    feat:declare types

---
+++ src/components/homepage/Workouts.tsx
@@ -0,0 +1,17 @@
+import { completeSoftNavigation } from "next/dist/client/components/segment-cache/navigation";
+import React from "react";
+
+const getWorkouts = async () => {
+  const response = await fetch("https://api.abcz.workers.dev/api/fitlog");
+  const data = await response.json();
+  return data;
+};
+
+const Workouts = async () => {
+  const workoutsData = await getWorkouts();
+  console.log(workoutsData);
+
+  return <div></div>;
+};
+
+export default Workouts;
